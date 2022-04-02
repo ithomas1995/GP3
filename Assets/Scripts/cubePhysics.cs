3477f7a29afe230adb6202e2f18911449112cf8b
unknown <raulplacido1180@knights.ucf.edu>
2022-04-02 09:55:06 -0400

    theatreAssets

---
+++ Assets/Scripts/cubePhysics.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubePhysics : MonoBehaviour
+{
+
+    public mouseTarget script;
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+    //  rigidbody= GetComponent<Rigidbody>();   
+    //  rigidbody.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(script.playerCut == true)
+        // {
+        //     // rigidbody.enabled = rigidbody.enabled;
+        // }
+    }
+}
