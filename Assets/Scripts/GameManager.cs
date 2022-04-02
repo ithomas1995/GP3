e666cf358a40a38ab62c531d8979535a2833809f
roolies <rilie.s@comcast.net>
2022-04-02 18:25:44 -0400

    theatrelevel

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool balloonFound;
+    public bool pictureFound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        balloonFound = false;
+        pictureFound = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
