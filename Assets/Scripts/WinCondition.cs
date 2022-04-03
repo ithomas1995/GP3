460ebedde7e83aac828c4c8c2931cb0268015226
ithomas1995 <ian.thomas0929@knights.ucf.edu>
2022-04-03 04:29:30 -0400

    vslive

---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    public Rigidbody rb;
+    public CapsuleCollider collider;
+    public Transform player;
+    public float pickUpRange;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distanceToPlayer = player.position - transform.position;
+                  
+        if (distanceToPlayer.magnitude <= pickUpRange && (Input.GetKeyDown(KeyCode.E))) PickUp();
+
+    }
+
+    private void PickUp()
+    {
+        
+        SceneManager.LoadScene("PlatformerDream");
+              
+    }
+    
+
+}
